f274e0c2d472e6cb0b2196c975433e6a341668a7
Benjamin Kluck <ben@fug.lu>
2019-02-28 19:46:08 +0100

    refactor: Remove consul

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import bunyan from "bunyan";
-import Consul from "consul";
 import express from "express";
 
 const logger = bunyan.createLogger({ name: "test-service:app" });
@@ -7,47 +6,23 @@ const logger = bunyan.createLogger({ name: "test-service:app" });
 logger.info("Starting up");
 
 const app = express();
-const consul = new Consul({ promisify: true });
 
-app.get("/health", (req, res) => {
-  res.send("ok");
+app.get("/", (req, res) => {
+  res.send({ time: Date.now() });
 });
 
 logger.info("Starting server");
-const server = app.listen(3000, async () => {
+const server = app.listen(3000, () => {
   logger.info("Listening on port 3000");
-  try {
-    logger.info("Registering service");
-    await consul.agent.service.register({
-      check: {
-        http: "http://localhost:3000/health",
-        interval: "2s"
-      },
-      name: "test-service",
-      port: 3000
-    });
-    logger.info("Registered service");
-  } catch (error) {
-    logger.error(error);
-    process.exit(1);
-  }
 });
 
-const shutdown = async () => {
-  logger.info("Catched shutdown signal");
-  try {
-    logger.info("Deregistering service");
-    await consul.agent.service.deregister("test-service");
-    logger.info("Deregistered service");
-  } catch (error) {
-    logger.error(error);
-  } finally {
-    logger.info("Closing server");
-    server.close(() => {
-      logger.info("Closed server");
-      process.exit(0);
-    });
-  }
+const shutdown = (signal: NodeJS.Signals) => {
+  logger.info(`Catched ${signal}`);
+  logger.info("Closing server");
+  server.close(() => {
+    logger.info("Closed server");
+    process.exit(0);
+  });
 };
 
 process.on("SIGTERM", shutdown);
